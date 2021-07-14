cd06c2a3adc696eabf6c7faba5e480ac4018f734
Douglas <dz@bitzend.net>
2021-07-14 11:35:53 -0600

    change  page name  for cancelled return

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ app.post('/create-checkout-session', async (req, res) => {
     ],
     // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
     success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${domainURL}/canceled.html`,
+    cancel_url: `${domainURL}/cancelled.html`,
   });
 
   return res.redirect(303, session.url);
