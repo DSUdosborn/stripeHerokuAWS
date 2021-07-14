7b651813047c3d81a420c354034a0cb75f14fb33
Douglas <dz@bitzend.net>
2021-07-14 11:56:23 -0600

    update stripe app info from sample info

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ checkEnv();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
   appInfo: { // For sample support and debugging, not required for production:
-    name: "stripe-samples/checkout-one-time-payments",
+    name: "naturistic e-store demo",
     version: "0.0.1",
-    url: "https://github.com/stripe-samples/checkout-one-time-payments"
+    url: "https://github.com/naturistic/herokuStripe"
   }
 });
 
