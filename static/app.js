b57456132ff1c50314f52b236a11b7235c632454
Douglas <dz@bitzend.net>
2021-07-14 23:21:56 -0600

    merge from client end of wednesday

---
+++ static/app.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        search_string:"",
+        products: [
+            {
+                "title":"Single Desert Mountain",
+                "url":"",
+                "image":"../images/desert_mountain.jpg",
+                "description":"Desert Mountain"
+            },
+            {
+                "title":"California Waves",
+                "url":"",
+                "image":"../images/wave.jpg",
+                "descriptioin":"Ocean Waves in California USA"
+            }
+        ]
+
+    },
+    vuetify: new Vuetify(),
+    computed: {
+        filteredProducts: function(){
+            var product_array = this.products;
+            var searchString=this.searchString
+            searchString = searchString.trim().toLowerCase();
+
+            product_array= product_array.filter(function(item){
+                if(item.title.toLowerCase().indexOf(searchString)!==-1){
+                    return item
+                }
+
+            })
+            return product_array
+        }
+   }
+});
