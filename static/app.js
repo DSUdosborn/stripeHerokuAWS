194581b7df7f101054e90cf7d06bebd3ee71bb31
Douglas <dz@bitzend.net>
2021-07-16 10:10:35 -0600

    remove  stripe from app.js

--- static/app.js
+++ static/app.js
@@ -22,29 +22,6 @@ var app = new Vue({
     methods:{
 
 
-        createStripeConnection: function(stripe_api_key){
-          const Stripe = require("stripe");
-          const stripe = Stripe(stripe_api_key);
-          stripe.setApiVersion('2019-03-14');//lock API version down to avoid code breaking
-          stripe.setAppInfo({
-            name: 'Naturistic',
-            version: "0.2.0", //Optional
-            url: 'https://naturistic.github.io' // Optional
-          });
-          stripe.setMaxNetworkRetries(3); //retry on network failure
-          return stripe;
-        },
-
-        getProducts: async function(){
-
-            console.log("getting all products");
-//            const stripe = createStripeConnection("rk_test_72wdhn7pifTOWbrtrSNFxhsQ00NrdzPvaC");
-            const stripe = require("stripe")("STRIPE_SECRET_KEY");
-            const products = await stripe.products.list({
-            limit: 15,
-          });
-
-        },
 
     },
 
